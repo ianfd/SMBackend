5cf9c6a8fab6361fbaaa80c5161c40a72606627d
CraftedCrime MAC <diks.ianferenc@gmail.com>
2021-03-19 11:18:55 +0100

    Initial commit

---
+++ src/main/java/de/ianfd/infrastructure/servermanagerwe/model/ServerObject.java
@@ -0,0 +1,9 @@
+package de.ianfd.infrastructure.servermanagerwe.model;
+/*
+ * Created by ian on 19.03.21
+ * Location: de.ianfd.infrastructure.servermanagerwe.model 
+ * Created for the project servermanagerwe with the name ServerObject
+*/
+
+public class ServerObject {
+}
